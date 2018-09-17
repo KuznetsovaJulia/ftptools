5e147c6fd79f7e4fc97ed97fe0c624c0ba842f93
Valeriy Mahanov <vmahanov@gmail.com>
2018-09-17 13:39:34 +0300

    Синхронизация справочника КТРУ

---
+++ app/commands/nsi/ktru/synchronize.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Nsi::Ktru::Synchronize
+  # Сервисный объект предназначенный для синхронизации справочника КТРУ
+  # Загрузка возможна как с ftp так и с локальной папки
+  # возможные параметры load_from: :files, загрузка из файлов, при этом необходимо так же задать массив строк files
+  include Interactor
+  include Interactor::Contracts
+
+  def call
+    SetContext.call context
+    Nsi::DownloadFromFtp.call(context) unless context.load_from == :files
+    # --- если есть что загружать - помечаем все записи в таблице как неактуальные
+    return unless context.files.empty?
+    context.model.update_all(actual: false)
+    Nsi::LoadFromFiles.call(context)
+  end
+end
